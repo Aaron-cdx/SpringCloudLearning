15dbf9071792899568b102644237bd268ee8e171
caoduanxi <cao236476>
2019-08-05 11:27:45 +0800

    消息总线的实现+rabbitmq的配置

--- springCloud-learn-character02/config-client/src/main/java/com/duanxi/configclient/ConfigClientApplication.java
+++ springCloud-learn-character02/config-client/src/main/java/com/duanxi/configclient/ConfigClientApplication.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 添加RefreshScope是RabbitMQ的相关操作
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+@RefreshScope
 @RestController
 public class ConfigClientApplication {
 
